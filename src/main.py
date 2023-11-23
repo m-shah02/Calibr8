4bb6d69fa61ee7d50aa3b7c085ebf2af75f0adfa
m-shah02 <mansoorshah488@gmail.com>
2023-11-23 12:52:00 +0000

    Cloned repo of example project

--- src/main.py
+++ src/main.py
@@ -1,16 +1,4 @@
-# This is a sample Python script.
+# Notes
+# Extrinsic = change of basis transformation from world coordinate system to camera coordinate system
+# Intrinsic = Projection transformation that projects points in the world viewed from camera onto image plane of camera
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Mansoor')
-    
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
